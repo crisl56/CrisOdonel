59850739a7566164bf9ea467caba807fec9d0ac5
Cris Odonel <crislaurenceodonel@gmail.com>
2026-08-14 20:53:50 -0700

    feat: added self images to about section

--- src/sections/Portfolio/AboutMe/AboutMe.tsx
+++ src/sections/Portfolio/AboutMe/AboutMe.tsx
@@ -2,7 +2,10 @@ import {useEffect, useRef, useState} from "react";
 import styles from "./AboutMe.module.css";
 
 const aboutImages = [
-    "/about/"
+    "/about/me1.jpg",
+    "/about/me2.jpg",
+    "/about/me3.jpg",
+    "/about/me4.jpg",
 ]
 
 export default function AboutMe(){
