5eed60d2183dd6706bfbad6c4a4c5bed6e1e4f14
Cris Odonel <crislaurenceodonel@gmail.com>
2026-08-14 22:33:05 -0700

    build: added second trailling slash for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <BrowserRouter basename="/CrisOdonel">
+      <BrowserRouter basename="/CrisOdonel/">
           <App />
       </BrowserRouter>
   </StrictMode>,
